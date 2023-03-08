6d6cbb0d944e9b823dd47ac91c5cf3a9d3b1be88
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-03-08 14:47:36 +0100

    [WEB/APP] React-query devtools

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import { type AppType } from "next/app";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
 import { Toaster } from "react-hot-toast";
 
 import { api } from "../utils/api";
 
-import "../styles/globals.css";
 import "../styles/fonts.css";
+import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -15,6 +16,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Toaster position="bottom-left" />
+      {process.env.NODE_ENV == "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
       <Component {...pageProps} />
     </SessionProvider>
   );
