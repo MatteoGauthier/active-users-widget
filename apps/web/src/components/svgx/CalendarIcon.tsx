a13d65a2065d8a9779cd03c4297c63a2fbf64598
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-03-13 16:32:01 +0100

    [WEB/UI] Add new icons

---
+++ apps/web/src/components/svgx/CalendarIcon.tsx
@@ -0,0 +1,24 @@
+import { SVGProps } from "react";
+
+export default function CalendarIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      >
+        <rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect>
+        <path d="M16 2v4M8 2v4m-5 4h18M8 14h.01M12 14h.01M16 14h.01M8 18h.01M12 18h.01M16 18h.01"></path>
+      </g>
+    </svg>
+  );
+}
---
+++ apps/web/src/components/svgx/GlobeIcon.tsx
@@ -0,0 +1,24 @@
+import { SVGProps } from "react";
+
+export default function GlobeIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      >
+        <path d="M21.54 15H17a2 2 0 0 0-2 2v4.54M7 3.34V5a3 3 0 0 0 3 3v0a2 2 0 0 1 2 2v0c0 1.1.9 2 2 2v0a2 2 0 0 0 2-2v0c0-1.1.9-2 2-2h3.17M11 21.95V18a2 2 0 0 0-2-2v0a2 2 0 0 1-2-2v-1a2 2 0 0 0-2-2H2.05"></path>
+        <circle cx="12" cy="12" r="10"></circle>
+      </g>
+    </svg>
+  );
+}
