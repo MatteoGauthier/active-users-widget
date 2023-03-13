417874e435323a83f2b8999b01820f6b690c3a61
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-03-13 16:31:09 +0100

    [WEB/UI] Add tremor in order to fasten UI integration

--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -3,7 +3,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
