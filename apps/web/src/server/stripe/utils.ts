fced1038d902953f6e30c8ed81264c814a896699
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-03-21 12:05:17 +0100

    [WEB/STRIPE] getAvailablePlans util

--- apps/web/src/server/stripe/utils.ts
+++ apps/web/src/server/stripe/utils.ts
@@ -1,3 +1,4 @@
+import { sift } from "radash";
 import { stripe } from "./client";
 
 export const createNewCustomer = async ({
@@ -13,4 +14,20 @@ export const createNewCustomer = async ({
   });
 
   return customer;
+};
+
+export const getAvailablePlans = async () => {
+  // @todo validate metadata
+  const productsResponse = (
+    await stripe.products.list({
+      active: true,
+    })
+  ).data;
+  const products = sift(
+    productsResponse.map((p) => ({
+      plan: p.metadata.plan_id,
+      productId: p.id,
+    }))
+  );
+  return products;
 };
