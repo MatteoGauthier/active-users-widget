e5ece9801ea5caf4529efc26ce5049a06208af69
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-04-23 15:56:39 +0200

    [WEB/UI] Success modal + confetti on setup success

--- apps/web/src/pages/dashboard/index.tsx
+++ apps/web/src/pages/dashboard/index.tsx
@@ -1,16 +1,37 @@
 import NewProjectCard from "@/components/dashboard/NewProjectCard";
 import ProjectCard from "@/components/dashboard/ProjectCard";
+import SetupSuccessModal from "@/components/dashboard/SetupSuccessModal";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import { api } from "@/utils/api";
+import { showSuccessConfettis } from "@/utils/visuals";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function DashboardPage() {
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
   const dashboardProjects = api.project.getUserDashboardProjects.useQuery();
 
   const projects = dashboardProjects.data?.projects;
   const code = dashboardProjects.data?.code;
 
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query?.setup === "true") {
+      showSuccessConfettis();
+      setShowSuccessModal(true);
+      setTimeout(() => {
+        router.replace({
+          pathname: "/dashboard",
+          query: {},
+        });
+      }, 100);
+    }
+  }, [router]);
+
   return (
     <DashboardLayout>
+      {showSuccessModal && <SetupSuccessModal />}
       <div>
         <h2 className="mt-8 mb-4 font-display text-3xl font-semibold">
           Projects
